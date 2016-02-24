77617f22f6bae8bd34d0870eaf2105113cdc934e
lanhnguyen <lanhnguyen@kms-technology.com>
2016-02-24 17:31:13 +0700

    database config

--- Readme.md
+++ Readme.md
@@ -2,7 +2,11 @@
 
 This sample application demonstrate how to create a desktop application using Angular JS and Github Electron.
 
+#Config database:
+edit Connection URL to database PostgreSQL in db/db.js and app/scripts/goods/goodsService.js
 
+-----
+    node db/db.js
 
 #Install
 --- 
@@ -30,6 +34,7 @@ npm install
 
 #Run 
 ---
+
 
 Run your application by entering following command in your command prompt
 
---
+++ db/db.js
@@ -0,0 +1,30 @@
+var pg = require('pg');
+var connectionString = "postgres://sonar:sonar@localhost/sonar";
+
+//this initializes a connection pool
+//it will keep idle connections open for a (configurable) 30 seconds
+//and set a limit of 20 (also configurable)
+pg.connect(connectionString, function(err, client, done) {
+  if(err) {
+    return console.error('error fetching client from pool', err);
+  }
+  client.query('SELECT $1::int AS number', ['1'], function(err, result) {
+    //call `done()` to release the client back to the pool
+    done();
+
+    if(err) {
+      return console.error('error running query', err);
+    }
+    console.log(result.rows[0].number);
+    //output: 1
+  });
+  var query = client.query('CREATE TABLE IF NOT EXISTS goods(id SERIAL PRIMARY KEY, name VARCHAR(200) not null, producer VARCHAR(100), unit VARCHAR(20), weight VARCHAR(20), price BIGINT)',  function(err2, result){
+
+  done();
+      if(err2) {
+        return console.error('error running query', err2);
+      }
+  console.log('done');
+  });
+
+});
